a02ea263b8043908159aca28c87ed1aa5831b898
anandsinghtanwar <71862526+anandsinghtanwar@users.noreply.github.com>
2021-02-05 03:09:58 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,43 @@
 //WAP to find the sum of n fractions.
+#include <stdio.h>
+#include <stdio.h>
+struct frac
+{
+    float a;
+    float b;
+};
+typedef struct frac Frac;
+Frac input()
+{
+    Frac f;
+    printf("Enter numerator:");
+    scanf("%f",&f.a);
+    printf("Enter denominator:");
+    scanf("%f",&f.b);
+    return f;
+}
+float compute(Frac f)
+{
+    float res;
+    res=(f.a / f.b); 
+    return res;
+}
+void output(float res)
+{
+    printf("The answer is: %f",res);
+}
+int main(void)
+{
+    int n;
+    float sum=0.0;
+    Frac f;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        f=input();
+        sum+=compute(f);
+    }
+    output(sum);
+    return 0;
+}
