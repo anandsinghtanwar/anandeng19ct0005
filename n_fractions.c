93de9327b091eae3004331ef2ffe03da78947440
anandsinghtanwar <71862526+anandsinghtanwar@users.noreply.github.com>
2021-03-11 02:37:16 +0530

    A10

--- n_fractions.c
+++ n_fractions.c
@@ -1,43 +1,73 @@
-//WAP to find the sum of n fractions.
 #include <stdio.h>
 #include <stdio.h>
 struct frac
 {
-    float a;
-    float b;
+    int a;
+    int b;
 };
-typedef struct frac Fr;
-Fr input()
+typedef struct frac Frac;
+Frac input()
 {
-    Fr f;
+    Frac f;
     printf("Enter numerator:");
-    scanf("%f",&f.a);
+    scanf("%d",&f.a);
     printf("Enter denominator:");
-    scanf("%f",&f.b);
+    scanf("%d",&f.b);
     return f;
 }
-float compute(Fr f)
+Frac add(Frac f,Frac result)
 {
-    float res;
-    res=(f.a / f.b); 
-    return res;
+ 
+   Frac add;
+   add.a=((f.a)*(result.b))+((result.a)*(f.b));
+   add.b=((f.b)*(result.b));
+   return add;
 }
-void output(float res)
+Frac gcd(Frac result)
 {
-    printf("The answer is: %f",res);
+    int gcd;
+    Frac simp;
+
+    for(int i=1; i <=result.a && i <= result.b; ++i)
+    {
+        if(result.a%i==0 && result.b%i==0)
+            gcd = i;
+    }
+    simp.a=result.a/gcd;
+    simp.b=result.b/gcd;
+    return simp;
+    
+}
+
+
+Frac output(Frac s)
+{
+    printf("The answer is: %d/%d",s.a,s.b);
 }
 int main(void)
 {
     int n;
     float sum=0.0;
-    Frac f;
+    Frac f,result,s;
     printf("Enter the value of n:");
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
-        f=input();
-        sum+=compute(f);
+        if(i==0)
+        {
+           result=input();
+        }
+        else
+        {
+           f=input();
+           result=add(f,result);
+           
+        }
+    
     }
-    output(sum);
+    
+    s=gcd(result);
+    output(s);
     return 0;
 }
+
